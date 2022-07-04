ae58af6efaf85557dd2a9930eedfa0fe58db7a74
priya <priyapandey8884.blr@gmail.com>
2022-07-04 15:52:48 +0530

    Ascending order

---
+++ src/Day03PracticeProblem/AscendingOrder.java
@@ -0,0 +1,35 @@
+package Day03PracticeProblem;
+
+public class AscendingOrder {
+
+	public static void main(String[] args) {
+		int[] array = new int[] {25,11,7,75,56};
+		int temp=0;
+		
+		//Displaying the original array
+		for(int i=0;i<array.length;i++) {
+			System.out.println(array[i]);
+		}
+		
+		//sort the array in ascending order
+		for(int i=0;i<array.length;i++) {
+			for(int j=i+1;j<array.length;j++) {
+				if(array[i] > array[j]) {
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+					}
+				}
+			
+		}
+		System.out.println();
+		
+		//display elements after sorting
+		System.out.println("elements of array sorted in ascending order");
+			for(int i=0;i<array.length;i++) {
+				System.out.print(array[i] + " ");
+			}
+
+	}
+
+}
